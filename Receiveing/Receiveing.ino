539eea443030198f10ba5d8ee99f132c35c7358b
RationalAsh <ashwinnarayan1994@gmail.com>
2013-09-14 13:19:22 +0530

    First commit

---
+++ Receiveing/Receiveing.ino
@@ -0,0 +1,53 @@
+/*
+-----Joystick data recieve code ver 1.00--------
+-----By Ashwin Narayan@RMI-NITT-----------------
+  
+  This is the side of the code that recieves the 
+  joystick data over the RF link. Upload this code
+  to the arduino on the PR2 which controls the camera
+  motion. Connect the data pin of the RF recieve
+  module to pin 11.
+  
+*/
+
+#include <VirtualWire.h>
+
+String sensorData;
+
+void setup()
+{
+  Serial.begin(9600); //Debugging
+  Serial.println("Setup");
+  pinMode(12, OUTPUT);
+  
+  vw_setup(2000); //Initialize communication at 2000 bits/sec
+  vw_rx_start();  //Start the receiver
+}
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  char receivedData[12];
+  
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+	int i;
+
+        digitalWrite(13, true); // Flash a light to show received good message
+	// Message with a good checksum received, dump it.
+	Serial.print("Got: ");
+	
+	for (i = 0; i < buflen; i++)
+	{
+	    Serial.print((char)buf[i]);
+            if(i<12) receivedData[i] = (char)buf[i];
+	    Serial.print(" ");
+	}
+	Serial.println("");
+        sensorData = (String)receivedData;
+        Serial.println(sensorData);
+        Serial.println("");
+        digitalWrite(13, false);
+    }
+}
---
+++ Transmitting/Transmitting.ino
@@ -0,0 +1,61 @@
+/*
+-----Joystick data transmission code ver 1.00---
+-----By Ashwin Narayan@RMI-NITT-----------------
+  
+  This is the transmission side of the the code.
+  Upload this code to the arduino which takes the
+  readings from the joystick. Connect the X reading
+  of the joystick to pin A0 and the Y-reading of the
+  joystick to pin A1. Connect the data pin of the RF
+  module to pin 12.
+  
+*/
+
+#include <VirtualWire.h>
+
+int JoyStickXPin = A0; //Connect Joystick x axis output to pin A0
+int JoyStickYPin = A1; //Connect Joystick y axis output to pin A1
+int SensorValX   = 0;  //Stores X sensor reading
+int SensorValY   = 0;  //Stores Y sensor reading
+int debugLEDPin  = 13; //For debugging
+
+String sensorData= "";
+const char *RF_Message = "Hello!";
+
+void setup()
+{
+  Serial.begin(9600); //Start serial monitor for debugging
+  pinMode(debugLEDPin, OUTPUT); //Set the LED pinmode to output
+  sensorData.reserve(11);
+  
+  vw_setup(2000);        //Setup the virtualwire library to transmit
+                         //at 2000 bits per second. Connect the data
+                         //pin of the transmitter module to pin 12 of
+                         //the arduino.
+}
+
+void loop()
+{
+  sensorData = "X";
+  SensorValX = analogRead(JoyStickXPin); //Read the values from
+  SensorValY = analogRead(JoyStickYPin); //the joystick
+  
+  //Pack the sensor data into a suitable format for sending over RF link
+  sensorData += SensorValX;
+  sensorData += ",";
+  sensorData += SensorValY;
+  sensorData += "Y";
+  //Print sensor data
+  Serial.println(sensorData);
+  //Convert to c string for sending over RF
+  RF_Message = sensorData.c_str();
+  
+  //Flash LED to show message is being sent
+  digitalWrite(debugLEDPin, true);
+  vw_send((uint8_t *)RF_Message, strlen(RF_Message)); //Send
+  vw_wait_tx(); //Wait until whole message is gone
+  digitalWrite(debugLEDPin, false);
+  
+  delay(10);
+}
+
